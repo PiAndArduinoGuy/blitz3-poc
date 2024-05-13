8d8f0e14653ee217aaaa2ff33c3f112aa91eaf24
quintin.kruger <quintin.kruger@multichoice.co.za>
2024-05-13 10:45:21 +0200

    Found a way to change the images of the colage page

--- src/pages/Colage.js
+++ src/pages/Colage.js
@@ -7,25 +7,53 @@ export default Blits.Component('Colage', {
   },
   template: `
     <Element w="1920" h="1080">
-    	<ImageTile w= "400" h="300" :for="(image, index) in $images" :src="$image.src" :x="$image.x" :y="$image.y" ref="image" :delay="$image.delay" :direction="$image.direction" />
+    	<ImageTile w= "400" h="300" :src="$imageTen" delay="1000" direction="up" />
+      <ImageTile w= "400" h="300" x="400" y="0" :src="$imageOne" delay="1000" />
+      <ImageTile w= "400" h="300" x="800" y="0" :src="$imageTwo" delay="2000" direction="up" />
+      <ImageTile w= "400" h="300" x="1200" y="0" :src="$imageThree" delay="3000" />
+      <ImageTile w= "400" h="300" x="0" y="300" :src="$imageFour" delay="4000" direction="up" />
+      <ImageTile w= "400" h="300" x="400" y="300" :src="$imageFive" delay="5000"  />
+      <ImageTile w= "400" h="300" x="800" y="300" :src="$imageSix" delay="6000" direction="up" />
+      <ImageTile w= "400" h="300" x="1200" y="300" :src="$imageSeven" delay="7000" />
+      <ImageTile w= "400" h="300" x="0" y="600" :src="$imageEight" delay="8000" direction="up" />
+      <ImageTile w= "400" h="300" x="400" y="600" :src="$imageNine" delay="9000" />
     </Element>
   `,
   state() {
     return {
-      images: [
-        { src: 'images/IMG_4349.JPEG', x: 0, y: 0, delay: '1000', direction: 'up' },
-        { src: 'images/IMG_4251.JPEG', x: 400, y: 0, delay: '2000' },
-        { src: 'images/IMG_4234.JPEG', x: 800, y: 0, delay: '3000', direction: 'up' },
-        { src: 'images/IMG_4212.JPG', x: 1200, y: 0, delay: '4000' },
-        { src: 'images/IMG_4089.JPEG', x: 0, y: 300, delay: '5000', direction: 'up' },
-        { src: 'images/IMG_4019.JPEG', x: 400, y: 300, delay: '6000' },
-        { src: 'images/IMG_3552.JPG', x: 800, y: 300, delay: '7000', direction: 'up' },
-        { src: 'images/IMG_1049.JPG', x: 1200, y: 300, delay: '8000' },
-        { src: 'images/IMG_1021.JPG', x: 0, y: 600, delay: '9000', direction: 'up' },
-        { src: 'images/IMG_0158.JPG', x: 400, y: 600, delay: '10000' },
-        { src: 'images/IMG_4550.JPEG', x: 800, y: 600, delay: '11000', direction: 'up' },
-        { src: 'images/IMG-5771.jpg', x: 1200, y: 600, delay: '12000' },
-      ],
+      imageOne: 'images/IMG_4349.JPEG',
+      imageTwo: 'images/IMG_4251.JPEG',
+      imageThree: 'images/IMG_4234.JPEG',
+      imageFour: 'images/IMG_4212.JPG',
+      imageFive: 'images/IMG_4089.JPEG',
+      imageSix: 'images/IMG_4019.JPEG',
+      imageSeven: 'images/IMG_3552.JPG',
+      imageEight: 'images/IMG_1049.JPG',
+      imageNine: 'images/IMG_1021.JPG',
+      imageTen: 'images/IMG_0158.JPG',
     }
+  },
+  hooks: {
+    ready() {
+      this.$setTimeout(() => {
+        console.log('Performing image tile updates')
+        this.updateImages()
+      }, 15000)
+    },
+  },
+
+  methods: {
+    updateImages() {
+      this.imageOne = 'images/IMG-7435.jpg'
+      this.imageTwo = 'images/scenic-1.jpg'
+      this.imageThree = 'images/scenic-2.jpg'
+      this.imageFour = 'images/scenic-3.jpg'
+      this.imageFive = 'images/scenic-4.jpg'
+      this.imageSix = 'images/scenic-5.jpg'
+      this.imageSeven = 'images/scenic-6.jpg'
+      this.imageEight = 'images/scenic-7.jpg'
+      this.imageNine = 'images/scenic-8.jpg'
+      this.imageTen = 'images/scenic-9.jpg'
+    },
   },
 })
